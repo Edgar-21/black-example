c9b4cafc60a810b46715e82b447d1f39e45e00a4
Edgar-21 <ejpflug@gmail.com>
2024-08-04 12:49:27 -0500

    apply black

--- test0.py
+++ test0.py
@@ -1,3 +1,7 @@
-def a_function_that_is_much_too_long(beep='boop', boop='beep', bop='bop', a_very_long_variable_name='chris'):
-    print("here is a very long string, I am going to wrap the text and leave the",
-          "parentesis in the wrong place")
+def a_function_that_is_much_too_long(
+    beep="boop", boop="beep", bop="bop", a_very_long_variable_name="chris"
+):
+    print(
+        "here is a very long string, I am going to wrap the text and leave th",
+        "parentesis in the wrong place",
+    )
